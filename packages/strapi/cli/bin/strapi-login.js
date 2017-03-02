bcbcb426351bb1867c1565d8177fbe369f96156b
Jim Laurie <j.laurie6993@gmail.com>
2017-03-02 14:12:11 +0100

    Create reusable process

--- packages/strapi/cli/bin/strapi-login.js
+++ packages/strapi/cli/bin/strapi-login.js
@@ -6,19 +6,8 @@
  * Module dependencies
  */
 
-// Node.js core
-const fs = require('fs');
-const path = require('path');
-
-// Signin form.
-const signinForm = require('../forms/signin');
-
-// Strapi services actions.
-const signinAction = require('../actions/authSignin');
-
-// Logger.
-const error = require('../utils/output/error');
-const success = require('../utils/output/success');
+// Login process.
+const loginProcess = require('../process/login');
 
 /**
  * `$ strapi login`
@@ -27,34 +16,7 @@ const success = require('../utils/output/success');
  */
 
 module.exports = async () => {
-  var url = 'http://localhost:1331';
+  await loginProcess();
 
-  async function action(email) {
-    const auth = await signinForm(email);
-
-    const res = await signinAction(url, auth);
-
-    if (res.error === 'invalid_password') {
-      error('Bad password');
-
-      action(auth.email);
-    } else if (res.error) {
-      error('Incorrect credentials');
-
-      action();
-    } else {
-      const HOME = process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'];
-
-      fs.writeFileSync(path.resolve(HOME, '.strapirc'), JSON.stringify({
-        email: res.profile.email,
-        token: res.token
-      }), 'utf8');
-
-      success(`You are successfully logged in as ${res.profile.fullname}`);
-
-      process.exit(1);
-    }
-  }
-
-  action();
+  process.exit(1);
 };
---
+++ packages/strapi/cli/process/login.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+// Node.js core
+const fs = require('fs');
+const path = require('path');
+
+// Signin form.
+const signinForm = require('../forms/auth');
+
+// Strapi services actions.
+const signinAction = require('../actions/authSignin');
+
+// Logger.
+const error = require('../utils/output/error');
+const success = require('../utils/output/success');
+
+module.exports = async () => {
+  const url = 'http://localhost:1331';
+
+  async function login(email) {
+    const auth = await signinForm(email);
+
+    const res = await signinAction(url, auth);
+
+    if (res.error === 'invalid_password') {
+      error('Bad password');
+
+      return await login(auth.email);
+    } else if (res.error) {
+      error('Incorrect credentials');
+
+      return await login();
+    } else {
+      const HOME = process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'];
+
+      fs.writeFileSync(path.resolve(HOME, '.strapirc'), JSON.stringify({
+        email: res.profile.email,
+        token: res.token
+      }), 'utf8');
+
+      success(`You are successfully logged in as ${res.profile.fullname}`);
+
+      return res;
+    }
+  }
+
+  return login();
+};
